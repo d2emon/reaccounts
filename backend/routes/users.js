9ee0346a6c1813f485611326d2fe3422041df161
Dmitry Kutsenko <d2emonium@gmail.com>
2018-06-04 22:11:51 +0300

    Add user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -10,4 +10,18 @@ router.get('/list', function(req, res) {
   });
 });
 
+/* POST to adduser. */
+router.post('/add', function(req, res) {
+  var account = new models.Account({
+    "user_id" : req.body.user_id,
+    "email" : req.body.useremail
+  });
+  account.save(function (err, doc) {
+      console.log(err)
+      res.send(
+        (err === null) ? { msg: '' } : { msg: err }
+      );
+  });
+});
+
 module.exports = router;
