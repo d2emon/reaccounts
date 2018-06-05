09c5c78d192f29914567f86544a31fc4d88c815b
Dmitry Kutsenko <d2emonium@gmail.com>
2018-06-05 22:18:02 +0300

    Removed docker shell

--- docker-run.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run --rm -it -p 8000:3000 reaccounts-backend
--- docker-shell.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run --rm -it -p 8000:3000 -v /home/d2emon/projects/node/Reaccounts/reaccounts:/app reaccounts-backend bash
