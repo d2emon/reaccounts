c4711ddb58ff8d42d55906789b79d4b9944fdd5c
Dmitry Kutsenko <d2emonium@gmail.com>
2018-06-16 01:27:44 +0300

    No CORS

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,14 @@ var router = express.Router();
 
 var models = require('../models');
 
+// Remove CORS
+router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
