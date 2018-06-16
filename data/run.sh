cd45f0144f0157850119b118ff3433e94ba9c626
Dmitry Kutsenko <d2emonium@gmail.com>
2018-06-16 19:00:58 +0300

    Loading fixtures

---
+++ data/run.sh
@@ -0,0 +1,8 @@
+echo "Running..."
+sleep $TIMEOUT
+MONGO_PATH="$MONGO_HOST:$MONGO_PORT/$MONGO_DB"
+echo "Connecting to $MONGO_PATH"
+mongo "$MONGO_PATH" --eval "db.dropDatabase();"
+echo "Database is clean"
+mongoimport --host "$MONGO_HOST" --port "$MONGO_PORT" --db "$MONGO_DB" --collection accounts --type json --file /fixtures/accounts.json --jsonArray
+echo "Data imported"
