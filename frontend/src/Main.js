155eac7ff9acae7a7f9895985b5882c49ad7a749
Dmitry Kutsenko <d2emonium@gmail.com>
2018-06-29 00:00:56 +0300

    Main toolbar

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -38,9 +38,8 @@ class Main extends Component {
   render () {
     return (
     <main>
-      <Container fluid={true}>
       <MainNavbar />
-      <Grid>
+      <Container fluid={true}>
         <Row>
           <Col xs={12}>
             <h1 onClick={this.clickHandler()}>
@@ -49,39 +48,41 @@ class Main extends Component {
           </Col>
         </Row>
         <Row>
-          <Col xs={6}>
+          <Col lg={3} xs={6}>
             <h1 onClick={this.clickHandler()}>
               {`Hello ${this.state.name}!`}
             </h1>
           </Col>
-          <Col xs={6}>
+          <Col lg={3} xs={6}>
             <h1 onClick={this.clickHandler()}>
               {`Hello ${this.state.name}!`}
             </h1>
           </Col>
-          <Col xs={6}>
+          <Col lg={3} xs={6}>
             <h1 onClick={this.clickHandler()}>
               {`Hello ${this.state.name}!`}
             </h1>
           </Col>
-          <Col xs={6}>
-            <h1 onClick={this.clickHandler()}>
-              {`Hello ${this.state.name}!`}
-            </h1>
-          </Col>
-          <Col xs={6}>
+          <Col lg= {3} xs={6}>
             <h1 onClick={this.clickHandler()}>
               {`Hello ${this.state.name}!`}
             </h1>
           </Col>
         </Row>
         <Row>
-          <Col xs={6}>
+          <Col md={12}>
             <h1 onClick={this.clickHandler()}>
               {`Hello ${this.state.name}!`}
             </h1>
           </Col>
-          <Col xs={6}>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <h1 onClick={this.clickHandler()}>
+              {`Hello ${this.state.name}!`}
+            </h1>
+          </Col>
+          <Col md={6}>
             <h1 onClick={this.clickHandler()}>
               {`Hello ${this.state.name}!`}
             </h1>
@@ -104,7 +105,7 @@ class Main extends Component {
             { /* <Route path='/roster' /> */ }
             { /* <Route exact path='/a-roster' /> */ }
             { /* </Switch> */ }
-            <p className="App-intro">
+	    <p className="App-intro">
               To get started, edit <code>src/App.js</code> and save to reload.
             </p>
           </Col>
@@ -125,26 +126,25 @@ class Main extends Component {
           <Col xs={6} md={4}>
             <code>&lt;{'Col xs={6} md={4}'} /&gt;</code>
           </Col>
-          <Col xsHidden md={4}>
+          <Col md={4}>
             <code>&lt;{'Col xsHidden md={4}'} /&gt;</code>
           </Col>
         </Row>
 
         <Row className="show-grid">
-          <Col xs={6} xsOffset={6}>
+          <Col xs={{ size: 6, offse: 6 }}>
             <code>&lt;{'Col xs={6} xsOffset={6}'} /&gt;</code>
           </Col>
         </Row>
 
         <Row className="show-grid">
-          <Col md={6} mdPush={6}>
+          <Col md={6}>
             <code>&lt;{'Col md={6} mdPush={6}'} /&gt;</code>
           </Col>
-          <Col md={6} mdPull={6}>
+          <Col md={6}>
             <code>&lt;{'Col md={6} mdPull={6}'} /&gt;</code>
           </Col>
         </Row>
-      </Grid>
       </Container>
     </main>
     )
--- frontend/src/MainNavbar.js
+++ frontend/src/MainNavbar.js
@@ -13,63 +13,59 @@ import {
   DropdownItem } from 'reactstrap';
 
 class MainNavbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render () {
     let notifications = [];
     for(let i=0; i<5; i++){
       notifications.push({
         id: i + 1,
         title: `Notification ${i + 1}`,
-        item: <DropdownItem eventKey={i + 1}>Notification {i + 1}</DropdownItem>
+        item: <DropdownItem key={i}>Notification {i + 1}</DropdownItem>
       });
     }
 
     return (
     <Navbar color="light" light expand="md">
-      <Navbar.Header>
-        <NavbarToggler onClick={this.toggle} />
-        <Navbar.Brand>
-          <a href="#home">Accounts</a>
-        </Navbar.Brand>
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-      </Navbar.Header>
+      {/*<Navbar.Header>*/}
+      <NavbarToggler onClick={this.toggle} />
+      <NavbarBrand href="#home">Accounts</NavbarBrand>
+      {/*</Navbar.Header>*/}
       <Collapse isOpen={this.state.isOpen} navbar>
         <Nav className="ml-auto" navbar>
-          <NavItem eventKey={1} href="#">
-            <p>Stats</p>
+          <NavItem>
+	    <NavLink href="#">
+              <p>Stats</p>
+	    </NavLink>
           </NavItem>
-          <DropdownMenu eventKey={2} title="Dropdown" id="basic-nav-dropdown">
-	    {notifications.map((notification, i) => notification.item)}
-            <DropdownItem divider />
-            <DropdownItem eventKey={2.9}>Separated link</DropdownItem>
-          </DropdownMenu>
-          <NavItem eventKey={3} href="#">
-            <p>Settings</p>
+	  <UncontrolledDropdown nav inNavbar>
+	    <DropdownToggle nav caret>
+              <span className="notification">{notifications.length}</span> Notifications
+            </DropdownToggle>
+	    <DropdownMenu right>
+	      {notifications.map((notification, i) => notification.item)}
+              <DropdownItem divider />
+              <DropdownItem>Another notification</DropdownItem>
+	    </DropdownMenu>
+          </UncontrolledDropdown>
+          <NavItem>
+	    <NavLink href="#">
+	      <p>Settings</p>
+	    </NavLink>
           </NavItem>
-
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-        </Nav>
+	</Nav>
       </Collapse>
     </Navbar>
     )
