3cd06c91bdf53c62785135e38519b508220c9eb9
Dmitry Kutsenko <d2emonium@gmail.com>
2018-07-15 22:04:07 +0300

    Main menu

---
+++ frontend/src/components/MainMenu.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+import FaSliders from 'react-icons/lib/fa/sliders';
+import FaBell from 'react-icons/lib/fa/bell';
+import FaCog from 'react-icons/lib/fa/cog';
+
+import Logo from './Logo';
+
+class MainNavbar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    renderSimpleItem (id, item) {
+        return (
+            <NavItem key={id}>
+                <NavLink href="#">
+                    { item.item }
+                </NavLink>
+            </NavItem>
+	)
+    }
+
+    renderComplexItem (id, item) {
+        return (
+            <UncontrolledDropdown nav inNavbar key={id}>
+                <DropdownToggle nav caret>
+		    { item.item }
+                </DropdownToggle>
+                <DropdownMenu right>
+                    { item.submenu.map((subitem, i) => subitem.item) }
+                    <DropdownItem divider />
+                    <DropdownItem>Another notification</DropdownItem>
+                </DropdownMenu>
+            </UncontrolledDropdown>
+	)
+    }
+
+    render () {
+        console.log(this.props.notifications);
+        return (
+            <Navbar color="light" light expand="md">
+                {/*<Navbar.Header>*/}
+                <NavbarToggler onClick={this.toggle} />
+                <NavbarBrand href="#home">
+                    <Logo
+                        style={{
+                            width: "30px",
+                            height: "30px"
+                        }}
+                    />
+                    Accounts
+                </NavbarBrand>
+                {/*</Navbar.Header>*/}
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        {this.props.items.map((item, i) => {
+                            console.log(item);
+                            return item.submenu ?
+	                        this.renderComplexItem(i, item) :
+				this.renderSimpleItem(i, item);
+			})}
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        )
+    }
+};
+
+export default MainNavbar;
+
--- frontend/src/containers/MainNavbar.js
+++ frontend/src/containers/MainNavbar.js
@@ -1,88 +1,45 @@
-import React, { Component } from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
-} from 'reactstrap';
+import React, { Component, Fragment } from 'react';
+import { DropdownItem } from 'reactstrap';
+
 import FaSliders from 'react-icons/lib/fa/sliders';
 import FaBell from 'react-icons/lib/fa/bell';
 import FaCog from 'react-icons/lib/fa/cog';
-import Logo from '../components/Logo';
+
+import MainMenu from '../components/MainMenu';
 
 class MainNavbar extends Component {
-  constructor(props) {
-    super(props);
+    constructor (props) {
+        super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-
-  render () {
-    let notifications = [];
-    for(let i=0; i<5; i++){
-      notifications.push({
-        id: i + 1,
-        title: `Notification ${i + 1}`,
-        item: <DropdownItem key={i}>Notification {i + 1}</DropdownItem>
-      });
+        let notifications_count = Math.floor(Math.random() * 10)
+        console.log(notifications_count);
+        let notifications = [];
+        for(let i = 0; i < notifications_count; i++){
+            notifications.push({
+                id: i + 1,
+                title: `Notification ${i + 1}`,
+                item: <DropdownItem key={i}>Notification {i + 1}</DropdownItem>
+            });
+        }
+        this.state = {
+            notifications: notifications
+        }
     }
 
-    return (
-    <Navbar color="light" light expand="md">
-      {/*<Navbar.Header>*/}
-      <NavbarToggler onClick={this.toggle} />
-      <NavbarBrand href="#home">
-        <Logo
-          style={{
-            width: "30px",
-	    height: "30px"
-	  }}
-        />
-        Accounts
-      </NavbarBrand>
-      {/*</Navbar.Header>*/}
-      <Collapse isOpen={this.state.isOpen} navbar>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-	    <NavLink href="#">
-              <FaSliders /> <p>Stats</p>
-	    </NavLink>
-          </NavItem>
-	  <UncontrolledDropdown nav inNavbar>
-	    <DropdownToggle nav caret>
-              <FaBell /> <span className="notification">{notifications.length}</span> Notifications
-            </DropdownToggle>
-	    <DropdownMenu right>
-	      {notifications.map((notification, i) => notification.item)}
-              <DropdownItem divider />
-              <DropdownItem>Another notification</DropdownItem>
-	    </DropdownMenu>
-          </UncontrolledDropdown>
-          <NavItem>
-	    <NavLink href="#">
-	      <FaCog /> <p>Settings</p>
-	    </NavLink>
-          </NavItem>
-	</Nav>
-      </Collapse>
-    </Navbar>
-    )
-  }
+    render () {
+        let menu = [
+            { item: <Fragment><FaSliders /> <p>Stats</p></Fragment> },
+            { item: (
+                <Fragment>
+                    <FaBell /> <span className="notification">{this.state.notifications.length}</span> Notifications
+		</Fragment>
+            ), submenu: this.state.notifications },
+            { item: <Fragment><FaCog /> <p>Settings</p></Fragment> }
+	]
+        return (
+            <MainMenu items={menu} />
+        )
+    }
 };
 
 export default MainNavbar;
