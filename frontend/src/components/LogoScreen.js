e6a77bbf05dd421e5ef97c3f1ff143e90c1ac822
Dmitry Kutsenko <d2emonium@gmail.com>
2018-07-21 22:56:57 +0300

    Logo screen as component

--- frontend/src/components/LogoScreen.js
+++ frontend/src/components/LogoScreen.js
@@ -1,8 +1,9 @@
 import React, {Fragment} from 'react';
-import {CreatedTime, ElapsedTime} from "../containers/GMain2Time";
+import Logo from './Logo';
 
 
 const LogoScreen = ({children, ...props}) => <Fragment>
+    <h1><Logo /></h1>
     <h1>A B E R  M U D</h1>
     <h2>By Alan Cox, Richard Acott Jim Finnis</h2>
     {children}
