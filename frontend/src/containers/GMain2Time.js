892fc3a266dc1ced0480d6f9bd26088509cdcb68
Dmitry Kutsenko <d2emonium@gmail.com>
2018-07-21 17:20:28 +0300

    Time refactored

--- frontend/src/containers/GMain2Time.js
+++ frontend/src/containers/GMain2Time.js
@@ -10,6 +10,13 @@ export function CreatedTime ({time, ...props}) {
  * Elapsed time and similar goodies
  */
 export function ElapsedTime ({time, ...props}) {
+    function Minutes ({minutes, ...props}) {
+        if (minutes === 1) {
+            return <span {...props}>1 minute</span>
+        }
+        return <span {...props}>{minutes} minutes</span>
+    }
+
     function Seconds ({r, ...props}) {
 	    let seconds = r % 60;
         if (seconds === 1) {
@@ -32,20 +39,19 @@ export function ElapsedTime ({time, ...props}) {
             return "Over a day!!!";
         }
         if (r < 61) { return <Seconds r={r} /> }
-        if (r === 60) { return <span>1 minute</span> }
-        if (r < 120) { return <span>1 minute and <Seconds r={r} /></span> }
+        if (r === 60) { return <span><Minutes minutes={1} />.</span> }
+        if (r < 120) { return <span><Minutes minutes={1} /> and <Seconds r={r} /></span> }
         if (r / 60 === 60) { return <span>1 hour</span> }
-        if (r < 3600) { return <span>{r / 60} minutes and <Seconds r={r} /></span> }
+        if (r < 3600) { return <span><Minutes minutes={r / 60} /> and <Seconds r={r} /></span> }
         if ((r / 60) % 60 !== 1) {
-            return <span><Hours r={r} /> {(r / 60) % 60} minutes.</span>
+            return <span><Hours r={r} /> <Minutes minutes={(r / 60) % 60} />.</span>
         } else {
-            return <span><Hours r={r} /> 1 minute</span>
+            return <span><Hours r={r} /> <Minutes minutes={1} />.</span>
         }
     }
 
-    if (!time) {
-        return <div {...props}>AberMUD has yet to ever start!!!</div>
-    }
-    return <div {...props}>Game time elapsed: {elapse(time)}</div>
+    return time
+        ? <div {...props}>Game time elapsed: {elapse(time)}</div>
+        : <div {...props}>AberMUD has yet to ever start!!!</div>;
 }
 
