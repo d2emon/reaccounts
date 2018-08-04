11d8154e85bdb46fe32c36dc06867c642e16b627
Dmitry Kutsenko <d2emonium@gmail.com>
2018-08-04 13:22:41 +0300

    Change password action

--- frontend/src/containers/Talker/ChangePasswordForm.js
+++ frontend/src/containers/Talker/ChangePasswordForm.js
@@ -12,133 +12,56 @@ import {
 
 import FormField from '../../components/FormField'
 
+import * as usersActions from '../../store/users/actions'
+
 class ChangePasswordForm extends Component {
     constructor (props) {
         super(props)
+
         this.state = {
-            fields: [
-
-                'oldPassword',
-                'newPassword',
-                'verifyPassword'
-            ],
-            oldPassword: {
-                value: '',
-                error: false
-            },
-            newPassword: {
-                value: '',
-                error: false
-            },
-            verifyPassword: {
-                value: '',
-                error: false
-            }
+            oldPassword: '',
+            newPassword: '',
+            verifyPassword: '',
         }
-
-        // this.handleInput = this.handleInput.bind(this)
-        this.changePassword = this.changePassword.bind(this)
     }
 
-    componentWillReceiveProps (nextProps, nextContext) {
-        console.log(nextProps)
-        let user = logscan(nextProps.username)
-
-        this.state.fields.forEach(fieldName => {
-            let field = this.state[fieldName]
-            if (!field) return
-
-            field.valid = true
-            field.error = false
-            if (!nextProps.errors) return this.setState({ [fieldName]: field })
-            // this.setField(field, nextProps.errors[field])
-
-            let error = nextProps.errors[fieldName]
-            if (!error) return this.setState({ [fieldName]: field })
-
-            console.error(fieldName, error)
-
-            if (error && field.error) return
-            field.valid = !error
-            field.error = error
-            this.setState({ [fieldName]: field })
-        })
-    }
-
-    validate (fieldName, value) {
-        this.setState({[fieldName]: {
-                value,
-                error: false
-            }}, () => {
-                // TODO: Move it to backend
-                /*
-                if (!this.props.user) throw Error('No user')
-                if (this.state.oldPassword.value !== this.props.user.password) throw Error('Incorrect Password')
-                if (!this.state.newPassword.value) throw Error('')
-                if (this.state.newPassword.value.indexOf(',')) throw Error('Illegal Character in password')
-                if (this.state.newPassword.value !== this.state.verifyPassword.value) throw Error('NO!')
-                */
-            })
-    }
-
-    valid () {
-        return !this.state.oldPassword.error
-            && !this.state.newPassword.error
-            && !this.state.verifyPassword.error
-    }
-
-    update (e) {
-        let field = this.state[e.target.name]
-        field.value = e.target.value
-        this.setState({ [e.target.name]: field })
-    }
+    update (e) { this.setState({ [e.target.name]: e.target.value }) }
 
     changePassword (e) {
         e.preventDefault()
-        // TODO: Move it to backend
-        this.validate('oldPassword', this.state.oldPassword.value)
-        this.validate('newPassword', this.state.newPassword.value)
-        this.validate('verifyPassword', this.state.verifyPassword.value)
-        console.log(
-            this.state.oldPassword.value,
-            this.state.newPassword.value,
-            this.state.verifyPassword.value
-        )
-        /*
-        delu2(this.props.username)
 
-        // delete me and tack me on end!
-        let fl = openlock(PFL, "a")
-        fl.fprintf(fl.qcrypt(this.props.user))
-        fl.fclose(fl)
-        */
+        this.props.dispatch(usersActions.changePassword({
+            user: this.props.user,
+            oldPassword: this.state.oldPassword,
+            newPassword: this.state.newPassword,
+            verifyPassword: this.state.verifyPassword
+        }))
     }
 
     render () {
         return <Form className="loginForm">
-            { /* onChange={this.handleInput} */ }
             <FormField
                 name="oldPassword"
                 label="Old Password"
-                error={this.state.oldPassword.error}
+                error={this.props.errors.oldPassword}
                 type="password"
                 onChange={this.update.bind(this)}
             />
             <FormField
                 name="newPassword"
                 label="New Password"
-                error={this.state.newPassword.error}
+                error={this.props.errors.newPassword}
                 type="password"
                 onChange={this.update.bind(this)}
             />
             <FormField
                 name="verifyPassword"
                 label="Verify Password"
-                error={this.state.verifyPassword.error}
+                error={this.props.errors.verifyPassword}
                 type="password"
                 onChange={this.update.bind(this)}
             />
-            <Button type="submit" color="primary" disabled={!this.valid()} onClick={this.changePassword}>Change</Button>
+            <Button type="submit" color="primary" onClick={ this.changePassword.bind(this) }>Change</Button>
         </Form>
     }
 }
@@ -148,6 +71,12 @@ function mapStateToProps(state) {
         // errors: errorsSelector.getErrors(state),
         // found: usersSelector.getUserFound(state),
         // user_exists: !!usersSelector.getUserFound(state)
+        user: 'logscan(${nextProps.username})',
+        errors: {
+            oldPassword: null,
+            newPassword: null,
+            verifyPassword: null
+        }
     }
 }
 
--- frontend/src/store/users/actions.js
+++ frontend/src/store/users/actions.js
@@ -99,3 +99,29 @@ export const searchUser = (user) => dispatch => {
             type: types.FOUND_USER
         }));
 };
+
+/**
+ * Change user password
+ * @param payload
+ * @returns {Function}
+ */
+export const changePassword = payload => dispatch => {
+    console.log('Action::changePassword', payload)
+    // TODO: Move it to backend
+    /*
+    if (!this.props.user) throw Error('No user')
+    if (this.state.oldPassword.value !== this.props.user.password) throw Error('Incorrect Password')
+    if (!this.state.newPassword.value) throw Error('')
+    if (this.state.newPassword.value.indexOf(',')) throw Error('Illegal Character in password')
+    if (this.state.newPassword.value !== this.state.verifyPassword.value) throw Error('NO!')
+    */
+
+    /*
+    delu2(this.props.username)
+
+    // delete me and tack me on end!
+    let fl = openlock(PFL, "a")
+    fl.fprintf(fl.qcrypt(this.props.user))
+    fl.fclose(fl)
+    */
+}
